70db3236f508536c40a880842f14571e8faa60ea
mohitchoudhary747 <mohitchoudhary1@outlook.com>
2021-12-19 12:22:13 +0530

    New Changes here in hello world file

--- src/udemy/java/randomcode/HelloWorld.java
+++ src/udemy/java/randomcode/HelloWorld.java
@@ -2,7 +2,6 @@ package udemy.java.randomcode;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World");
-        System.out.println("Hello World2");
+        System.out.println("Hello Mohit");
     }
 }
