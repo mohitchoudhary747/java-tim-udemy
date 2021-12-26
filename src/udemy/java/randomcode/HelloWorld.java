70630b5d9a3c1f65e8439eadfbd7ecc047a4ca2c
mohitchoudhary747 <mohitchoudhary1@outlook.com>
2021-12-26 23:36:50 +0530

    new sample chnages

--- src/udemy/java/randomcode/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package udemy.java.randomcode;
-
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello Mohit");
-    }
-}
---
+++ src/udemy/java/randomcode/Operators.java
@@ -0,0 +1,15 @@
+package udemy.java.randomcode;
+
+public class Operators {
+    public static void main(String[] args){
+        System.out.println("Hello Mohit");
+
+        // Turnary Operator
+       int number=20;
+       boolean isEightheenAbove=number>18;
+        System.out.println("Is boy elder than 18? -> "+isEightheenAbove);
+        System.out.println();
+        System.out.println("Mohit ");
+        //loal chnages
+    }
+}
